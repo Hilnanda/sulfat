20e455f6d70d4d21683802fa7379dd8865f22769
Muhamad Aldi Rifai <37617582+aldirifai@users.noreply.github.com>
2022-07-05 18:48:16 +0700

    redirect login

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PeramalanController;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\PermintaanController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,9 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('/dashboard');
+    }
     return view('login');
 })->name('login');
 
